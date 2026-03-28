7c27728d7e7b380bd7859c53d9eb96bb8d18ae38
dcodetuyyi <tuyyistore@gmail.com>
2026-03-28 23:19:47 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ export const config = {
     botNumber: "6281913824200",      // ganti dengan nomor bot kamu (contoh 6281234567890)
     ownerNumber: "6283121214520",    // nomor owner
     prefix: ".",
-    apiKey: "tuyyisky",   // isi api key dari neoxr
+    apiKey: "",   // isi api key dari neoxr
     sessionName: "session",
     botName: "My Bot"
 }
